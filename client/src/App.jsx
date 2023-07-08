34e961e50c9aae4717a74fcd51fbc1f012d81172
Byekibe <kibetpeter95@gmail.com>
2023-07-09 00:14:08 +0300

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import HomePage from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header.jsx';
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+};
+
+export default HomePage;
