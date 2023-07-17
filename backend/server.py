399f74893ff31d8a40532e42e7099dc79e53f48b
Byekibe <kibetpeter95@gmail.com>
2023-07-17 23:58:02 +0300

    Send mail using SMTP

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 load_dotenv()
 import os
 
 PORT = os.getenv('PORT')
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
@@ -19,6 +21,14 @@ def contact():
 @app.route("/date_year")
 def date_year():
     return { "msg": "Year" }
+
+@app.route("/mail", methods=["GET", "POST"])
+def mail():
+    contact_name = request.json.get("name")
+    email = request.json.get("email")
+    message = request.json.get("message")
+    print(contact_name, email, message)
+    return { "msg": f"Thanks {contact_name} your message has been sent!" }
 
 
 if __name__=="__main__":
