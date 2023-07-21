e686f1a50091edfe70a614389c3b6eb839d511e5
Byekibe <kibetpeter95@gmail.com>
2023-07-21 21:23:29 +0300

    Hamburger menu correction

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container">
             <Link to="/" className="navbar-brand my-font" href="#">Peter</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
