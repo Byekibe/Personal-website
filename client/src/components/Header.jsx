47b26d821569ee83a1acc92aef00fd77e8198709
Byekibe <kibetpeter95@gmail.com>
2023-11-13 03:00:17 +0300

    confirm commits about api calls

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,37 +1,64 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
-    const [theme, setTheme] = useState(false);
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container">
-            <Link to="/" className="navbar-brand my-font" href="#">Peter</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" target='_blank' to="http://blog.byedev.com">Blog</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="/contacts">Contact</Link>
-                </li>
-                <li className="nav-item">
-                <Link to="/hire" className="nav-link">Build products</Link>
-                </li>
-            </ul>
-            </div>
+  const [theme, setTheme] = useState(false);
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <Link to="/" className="navbar-brand my-font" href="#">
+          Peter
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                target="_blank"
+                to="http://blog.byedev.com"
+              >
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contacts">
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/hire" className="nav-link">
+                Build products
+              </Link>
+            </li>
+          </ul>
         </div>
-        </nav>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
